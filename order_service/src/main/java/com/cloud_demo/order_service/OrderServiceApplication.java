2f8ff002afd6a859f80585829ed1acc352a22a27
yasinshaw <yasinshaw@gmail.com>
2018-08-13 19:40:53 +0800

    add annotation to enable eureka client

--- order_service/src/main/java/com/cloud_demo/order_service/OrderServiceApplication.java
+++ order_service/src/main/java/com/cloud_demo/order_service/OrderServiceApplication.java
@@ -2,7 +2,9 @@ package com.cloud_demo.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderServiceApplication {
 
