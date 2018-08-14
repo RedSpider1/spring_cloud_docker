652585d6868e5d272835822a89124bc5a84b4c6b
chenxiao <chenxiao19920206@163.com>
2018-08-14 23:30:24 +0800

    test commit2

--- eureka_server/src/main/java/com/cloud_demo/eureka_server/EurekaServerApplication.java
+++ eureka_server/src/main/java/com/cloud_demo/eureka_server/EurekaServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
+        //test commit by chenxiao
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
