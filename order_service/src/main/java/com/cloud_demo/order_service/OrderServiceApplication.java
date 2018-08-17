5d9da72921e64d1a58eeb3e3855ad0c6a0bd79b7
yasinshaw <yasinshaw@gmail.com>
2018-08-17 16:54:44 +0800

    remove unused import class

--- order_service/src/main/java/com/cloud_demo/order_service/OrderServiceApplication.java
+++ order_service/src/main/java/com/cloud_demo/order_service/OrderServiceApplication.java
@@ -1,17 +1,9 @@
 package com.cloud_demo.order_service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.health.HealthIndicatorAutoConfiguration;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableFeignClients
