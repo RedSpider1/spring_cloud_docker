67d6169f859cbb46964d7242cba86b96ef3ed768
yasinshaw <yasinshaw@gmail.com>
2018-08-18 23:40:07 +0800

    remove unused imported classes

--- order_service/src/main/java/com/cloud_demo/order_service/feign/ProductFeignClient.java
+++ order_service/src/main/java/com/cloud_demo/order_service/feign/ProductFeignClient.java
@@ -4,8 +4,6 @@ import com.cloud_demo.order_service.entity.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author Yasin Shaw
