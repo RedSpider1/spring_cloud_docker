fcb8763c2febe00e2485c260b7070325911ec8f1
yasinshaw <yasinshaw@gmail.com>
2018-08-18 22:45:53 +0800

    add interface of product service

---
+++ product_service/src/main/java/com/example/product_service/entity/Product.java
@@ -0,0 +1,38 @@
+package com.example.product_service.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Yasin Shaw
+ * @version v1.0
+ */
+public class Product {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
