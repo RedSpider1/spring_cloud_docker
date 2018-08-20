751ad6b9a5f215465e1f09556858fa8f866351a4
yasinshaw <yasinshaw@gmail.com>
2018-08-20 21:38:50 +0800

    add user service module

---
+++ user_service/src/main/java/com/cloud_demo/user_service/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.cloud_demo.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
