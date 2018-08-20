81271a3450551ebae6c1b5f589d0344a1a625c72
yasinshaw <yasinshaw@gmail.com>
2018-08-20 21:35:06 +0800

    update annotation to EnableDiscoveryClient

--- product_service/src/main/java/com/example/product_service/ProductServiceApplication.java
+++ product_service/src/main/java/com/example/product_service/ProductServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
