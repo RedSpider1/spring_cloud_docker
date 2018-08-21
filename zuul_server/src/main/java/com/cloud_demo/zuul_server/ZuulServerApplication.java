d7f4aa02a6ae9c1da6d053120dccfdaae45a38a1
yasinshaw <yasinshaw@gmail.com>
2018-08-21 22:25:28 +0800

    update configuration

--- zuul_server/src/main/java/com/cloud_demo/zuul_server/ZuulServerApplication.java
+++ zuul_server/src/main/java/com/cloud_demo/zuul_server/ZuulServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.zuul_server;
+package com.cloud_demo.zuul_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- zuul_server/src/test/java/com/cloud_demo/zuul_server/ZuulServiceApplicationTests.java
+++ zuul_server/src/test/java/com/cloud_demo/zuul_server/ZuulServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.zuul_server;
+package com.cloud_demo.zuul_server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
